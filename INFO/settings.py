c442b0d13919b1619171c76b6154ba4bcb7a58ac
rohitmechanical <rohit_mechanical@yahoo.com>
2022-08-19 23:23:42 +0530

    Third

--- INFO/settings.py
+++ INFO/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lg#-phg!!7z(^334v)#b(rvthcb!d6jjcu*g=g9*v(x)qik2t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://armelock.herokuapp.com/']
 
 
 # Application definition
@@ -128,7 +128,7 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR/'static'),)
 MEDIA_URL='/image/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/image/')
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR/'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
